875053772ac3da28a0dfe868c76894eb9ff4e87e
liqiubang <15274438093@163.com>
2025-05-13 16:58:22 +0800

    post请求

--- api/test_api.py
+++ api/test_api.py
@@ -5,14 +5,15 @@ from api.yaml_util import YamlUtil
 
 
 class TestApi:
-    @pytest.mark.parametrize("args",  YamlUtil('test_api.yaml').read_yaml())
-    def test_01_api(self,args):
+    @pytest.mark.parametrize("args", YamlUtil('test_api.yaml').read_yaml())
+    def test_01_api(self, args):
         url = args["request"]["url"]
-        params = args["request"]["params"]
-        res = requests.get(url, params=params)
-        print(res.text)
-        #断言
-        assert args["validate"]["eq"]["code"] in res.text
+        json = args["request"]["json"]
+        res = requests.post(url, json=json)  # get用params
+        print(f"状态码: {res.status_code}\n响应内容: {res.text}\n请求URL: {url}\n请求参数: {json}")  # ✔️ 中文标签
+
+        # 断言
+        # assert args["validate"]["eq"]["code"] in res.text
 
 
 if __name__ == '__main__':
